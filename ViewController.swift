9f89942b1c7367bf58cb405588da2452593dd789
n8davis@me.com <n8davis@me.com>
2019-03-24 13:57:24 -0500

    First Commit

---
+++ ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  i-am-rich
+//
+//  Created by Nate Davis on 3/21/19.
+//  Copyright © 2019 Nate Davis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
